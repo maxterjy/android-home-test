362d00daf58d4a781262b4f37bafaebdea0d1f84
maxterjy <maxterjy@gmail.com>
2019-12-24 18:22:08 +0700

    Add category fragment

--- HomeTest/app/src/main/java/thachpham/hometest/home/HomeActivity.java
+++ HomeTest/app/src/main/java/thachpham/hometest/home/HomeActivity.java
@@ -1,24 +1,21 @@
 package thachpham.hometest.home;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import thachpham.hometest.R;
-import thachpham.hometest.hotkey.HotkeyFragment;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-        fragmentTransaction.replace(R.id.fragment_container, new HotkeyFragment());
-        fragmentTransaction.commit();
+//        FragmentManager fm = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fm.beginTransaction();
+//        fragmentTransaction.replace(R.id.fragment_container, new HotkeyFragment());
+//        fragmentTransaction.commit();
     }
 }

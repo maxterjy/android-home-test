170f8ee44a52154e58857af7ac68986c4024afe0
maxterjy <maxterjy@gmail.com>
2019-12-24 07:55:18 +0700

    Convert main menu fragment to data binding layout

--- HomeTest/app/src/main/java/thachpham/hometest/fragment/MainMenuFragment.java
+++ HomeTest/app/src/main/java/thachpham/hometest/fragment/MainMenuFragment.java
@@ -4,6 +4,7 @@ package thachpham.hometest.fragment;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
@@ -18,11 +19,13 @@ import java.util.List;
 
 import thachpham.hometest.DLog;
 import thachpham.hometest.R;
+import thachpham.hometest.databinding.FragmentMainMenuBinding;
 import thachpham.hometest.viewmodel.MainMenuViewModel;
 
 public class MainMenuFragment extends Fragment {
 
     MainMenuViewModel mViewModel;
+    FragmentMainMenuBinding mBinding;
 
     public MainMenuFragment() {
 
@@ -47,8 +50,9 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_main_menu, container, false);
 
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        return mBinding.getRoot();
     }
 
 }
